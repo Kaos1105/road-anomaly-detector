8281ceb29d430d1947cd2ceb1e4bf15f104251c5
KAOS <danhnt1105@gmail.com>
2025-04-02 00:15:41 +0700

    clean up code

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import threading
 from server import create_app
 
 app = create_app()
 
 def start_server():
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
 if __name__ == '__main__':
     # threading.Thread(target=start_server, daemon=True).start()
     start_server()
-    
-    # Keep app running
-    while True:
-        pass
