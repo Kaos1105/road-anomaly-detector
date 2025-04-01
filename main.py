8887e3f1cc315dc7974c90897477cb834885b2a8
KAOS <danhnt1105@gmail.com>
2025-04-02 01:15:29 +0700

    update with GUI

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from server import create_app
+from kivy.uix.label import Label
+from kivy.app import App
+from threading import Thread
 
 app = create_app()
 
 def start_server():
     app.run(host='0.0.0.0', port=5000, debug=False)
 
+class MyApp(App):
+    def build(self):
+        return Label(text="Flask is running!")
+    
 if __name__ == '__main__':
-    # threading.Thread(target=start_server, daemon=True).start()
-    start_server()
+    Thread(target=start_server, daemon=True).start()
+    MyApp().run()
